f388f42d4a2a56dfcbcc8c56b0f4b4aab4c92c57
Manish Kumar <manish.rajput62647457@gmail.com>
2026-06-02 16:03:05 +0530

    exception-filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { ValidationPipe, Logger } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './common/filters/http-exception-filter';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { CleanupService } from './services/cleanup.service';
 import { PinoLogger } from './common/utils/pino-logger';
